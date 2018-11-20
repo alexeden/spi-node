14af5016410ceab8c50592a992c1b0c96b9c67a4
Alex Eden <alexandereden91@gmail.com>
2018-11-20 13:35:39 -0600

    Add static constructors and chainable  setter methods to SPI

--- src/spi.ts
+++ src/spi.ts
@@ -1,29 +1,53 @@
 import * as fs from 'fs';
-import { Mode, Order, SpiAddon } from './spi.types';
+import { Mode, Order, SpiAddon, Settings, TransferFunction } from './spi.types';
 import { constraints } from './utils';
 
 // tslint:disable-next-line:no-var-requires
 const SpiAddon: SpiAddon = require('bindings')('spi');
 
-export class SPI {
+export class SPI implements Settings {
   static readonly spiSupported = SpiAddon.spiSupported;
 
-  private fd: number;
+  static fromDevicePath(devicePath = '/dev/spidev0.0') {
+    const fd = fs.openSync(devicePath, 'r+');
+    return new SPI(fd);
+  }
+
+  static fromFileDescriptor(fd: number) {
+    return new SPI(fd);
+  }
 
   @constraints(x => Object.values(Mode).includes(x))
   mode: Mode = Mode.M0;
+  setMode(mode: Mode) {
+    this.mode = mode;
+    return this;
+  }
 
   @constraints(Number.isSafeInteger, x => x > 0)
   speed = 4e6;
+  setSpeed(speed: number) {
+    this.speed = speed;
+    return this;
+  }
 
   @constraints(x => Object.values(Order).includes(x))
   order: Order = Order.MSB_FIRST;
-
-  constructor(
-    readonly devicePath = '/dev/null'
-  ) {
-    this.fd = fs.openSync(this.devicePath, 'r+');
+  setOrder(order: Order) {
+    this.order = order;
+    return this;
   }
+
+  @constraints(x => x === null || typeof x === 'function')
+  transferOverride: TransferFunction | null = null;
+  setTransferOverride(txFn: TransferFunction) {
+    this.transferOverride = txFn;
+    return this;
+  }
+
+  private constructor(
+    readonly fd: number
+  ) {}
 
   write(buffer: Buffer) {
     return this.transfer(buffer, 0);
